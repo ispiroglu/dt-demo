106f44e8e2b8ac9d1b9c76fa9856105b93a1cb70
evren.ispiroglu <evrenn.ispiroglu@gmail.com>
2022-09-09 13:32:26 +0300

    model dev

---
+++ src/main/kotlin/com/ispiroglu/dtdemo/DtDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ispiroglu.dtdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DtDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DtDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ispiroglu/dtdemo/DtDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ispiroglu.dtdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DtDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
